98391b27ec61197ed47c1d0134abf33ad1cec343
Kim Minsung <epicblue@hanmail.net>
2021-08-17 13:26:36 +0900

    home page index to jsp file

---
+++ HelloJSP/src/main/java/com/example/emaillist/controller/HomeServlet.java
@@ -0,0 +1,21 @@
+package com.example.emaillist.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="home",urlPatterns="/")
+public class HomeServlet extends HttpServlet{
+@Override
+protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	// 홈페이지로 포워딩
+	RequestDispatcher rd = req.getServletContext().getRequestDispatcher("/WEB-INF/views/home.jsp");
+	rd.forward(req, res);
+	
+}
+}
